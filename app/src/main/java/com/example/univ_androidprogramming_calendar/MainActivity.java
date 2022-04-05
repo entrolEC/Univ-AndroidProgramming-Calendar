13ce15e5babd0cab351c92a42a302cd58e3d6cf9
qoquma <euichan1108@gmail.com>
2022-04-05 22:32:15 +0900

    Move Calendar

--- app/src/main/java/com/example/univ_androidprogramming_calendar/MainActivity.java
+++ app/src/main/java/com/example/univ_androidprogramming_calendar/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.univ_androidprogramming_calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -14,13 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GridView gridview = (GridView) findViewById(R.id.gridview);
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2022,1,5);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+
+        Intent intent = getIntent();
+        int year = intent.getIntExtra("year", calendar.get(Calendar.YEAR));
+        int month = intent.getIntExtra("month", calendar.get(Calendar.MONTH));
+        calendar.set(year, month, calendar.get(Calendar.DATE));
+
         gridview.setAdapter(new CalendarAdapter(this, calendar));
 
         TextView title = findViewById(R.id.textView_title);
         title.setText(String.format("%d.%d",calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH)+1));
+
+        Button prev = findViewById(R.id.button_prev);
+        Button next = findViewById(R.id.button_next);
+
+        prev.setOnClickListener(new moveCalendar(calendar, -1));
+        next.setOnClickListener(new moveCalendar(calendar, 1));
+
+
+    }
+
+    class moveCalendar implements View.OnClickListener {
+        private Calendar calendar;
+        private int value;
+
+        moveCalendar(Calendar calendar, int value) {
+            this.calendar = calendar;
+            this.value = value;
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            calendar.add(Calendar.MONTH, value);
+            intent.putExtra("year", calendar.get(Calendar.YEAR));
+            intent.putExtra("month", calendar.get(Calendar.MONTH));
+            startActivity(intent);
+            finish();
+        }
     }
 }
