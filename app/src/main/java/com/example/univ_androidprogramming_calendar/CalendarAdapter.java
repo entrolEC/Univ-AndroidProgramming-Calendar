c69b20d536e5ba7fd6785c2b3766fbaf061a6576
PRESSO <user1205@naver.com>
2022-05-08 19:40:38 +0900

    Highlight Selected Item

--- app/src/main/java/com/example/univ_androidprogramming_calendar/CalendarAdapter.java
+++ app/src/main/java/com/example/univ_androidprogramming_calendar/CalendarAdapter.java
@@ -17,7 +17,6 @@ import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
 
 import java.util.Calendar;
 
@@ -25,13 +24,14 @@ public class CalendarAdapter extends BaseAdapter {
     private Context mContext;
     private Calendar calendar;
     private int minDate = 0;
+    private TextView lastSelected;
 
     public CalendarAdapter(Context mContext, Calendar calendar) {
         this.mContext = mContext;
         this.calendar = calendar;
-        this.calendar.set(Calendar.MONTH, 6);
+
         calendar.set(Calendar.DAY_OF_MONTH, 1);
-        minDate = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+        this.minDate = calendar.get(Calendar.DAY_OF_WEEK) - 1;
     }
 
     @Override
@@ -65,25 +65,29 @@ public class CalendarAdapter extends BaseAdapter {
         }
 
         textView.setGravity(Gravity.CENTER_HORIZONTAL);
-        // textView.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1f));
-//        int height = ((Activity) mContext).getWindowManager().getDefaultDisplay().getHeight();
+
         int width = ((Activity) mContext).findViewById(R.id.fragment).getWidth();
         int height = ((Activity) mContext).findViewById(R.id.fragment).getHeight();
         textView.setLayoutParams(new GridView.LayoutParams(width / 7, height / 6));
 
         // https://stackoverflow.com/questions/12523005/how-set-background-drawable-programmatically-in-android
-        textView.setBackground(ContextCompat.getDrawable(mContext, R.drawable.border));
+        textView.setBackgroundResource(R.drawable.border);
 
-        if (i - minDate + 1 > 0) {
+        if (0 <= i - minDate && i - minDate + 1 <= calendar.getActualMaximum(Calendar.DAY_OF_MONTH)) {
             textView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    if (lastSelected != null) {
+                        lastSelected.setBackgroundResource(R.drawable.border);
+                    }
+                    lastSelected = textView;
+                    textView.setBackgroundColor(Color.CYAN);
+
                     String msg = calendar.get(Calendar.YEAR) + "." + (calendar.get(Calendar.MONTH) + 1) + "." + Integer.toString(i - minDate + 1);
                     Toast.makeText(mContext.getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
                 }
             });
         }
-
 
         return textView;
     }
