ec3a8922e86755cd85806e78d69ca9170aa76c56
euichan2 <euichan1108@gmail.com>
2022-06-06 20:49:11 +0900

    AddSchedule form

--- app/src/main/java/com/example/univ_androidprogramming_calendar/AddScheduleActivity.java
+++ app/src/main/java/com/example/univ_androidprogramming_calendar/AddScheduleActivity.java
@@ -1,14 +1,105 @@
 package com.example.univ_androidprogramming_calendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddScheduleActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+public class AddScheduleActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mGoogleMap = null;
+    private String searchPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button button = findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.search_editText);
+                searchPlace = editText.getText().toString();
+
+                Geocoder geocoder = new Geocoder(getBaseContext()); //https://salix97.tistory.com/124
+                List<Address> addresses = null;
+
+                try {
+                    addresses = geocoder.getFromLocationName(searchPlace, 3);
+                    if (addresses != null && !addresses.equals(" ")) {
+                        search(addresses);
+                    }
+                } catch(Exception e) {
+
+                }
+
+            }
+        });
+    }
+
+    protected void search(List<Address> addresses) {
+        Address address = addresses.get(0);
+        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude()); // 주소
+
+        String addressText = String.format(
+                "%s, %s",
+                address.getMaxAddressLineIndex() > 0 ? address
+                        .getAddressLine(0) : " ", address.getFeatureName());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title(addressText);
+
+        mGoogleMap.clear();
+        mGoogleMap.addMarker(markerOptions);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        LatLng hansung = new LatLng(37.5817891, 127.009854);
+        googleMap.addMarker(
+                new MarkerOptions().
+                        position(hansung).
+                        title("한성대학교"));
+
+        // move the camera
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hansung,15));
+
+        mGoogleMap.setOnMarkerClickListener(new MyMarkerClickListener());
+    }
+
+    class MyMarkerClickListener implements GoogleMap.OnMarkerClickListener {
+
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            if (marker.getTitle().equals(searchPlace)) {
+                Toast.makeText(getApplicationContext(),"한성대입구역을 선택하셨습니다", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
     }
 }
